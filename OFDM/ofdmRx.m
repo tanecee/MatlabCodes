0536a398f3b0814523d8ea708c8fc71560a201d5
Ece Tan <143082117+tanecee@users.noreply.github.com>
2025-08-26 16:41:05 +0300

    Update ofdmRx.m

--- OFDM/ofdmRx.m
+++ OFDM/ofdmRx.m
@@ -1,19 +1,25 @@
 function [ber, num_errors, rx_bits, rx_data_symbols] = ofdmRx(rx_data, tx_bits)
 p = parametersOFDM;
 
+%xcorr ile ZC preamble’ı aranır ve 
+%En güçlü pik bulunduğunda, onun bittiği noktadan başlayarak OFDM dalgası kesilir.
 [corr, lag] = xcorr(rx_data, p.sync);
 [~, max_idx] = max(abs(corr));
 start_idx = lag(max_idx) + length(p.sync) + 1;
 
 if start_idx < 1 || start_idx + p.wformLength - 1 > length(rx_data)
-    warning('Senkronizasyon indeksi geçersiz, veriyi başlangıçtan itibaren kullanıyorum.');
+    warning('Senkronizasyon indeksi geçersiz.');
     start_idx = 1; % Başlangıçtan itibaren al
-    rx_data = rx_data(1:p.wformLength); % İlk wformLength kadar veri al
+    rx_data = rx_data(1:p.wformLength); 
 else
     rx_data = rx_data(start_idx:start_idx + p.wformLength - 1);
 end
 
-% Residual CFO estimation using CP
+%CP ile sembolün başı ve sonu aynı olduğundan, aradaki faz farkı CFO’ya karşılık gelir.
+%Formül: Δf ≈ angle / (2π) * Fs / Nfft kodda: mean(phase_offsets)/(2π*Nfft)*Fs
+%median(...): tekil sıçramalara dayanıklı faz tahmini.
+%Bulunan Δf (Hz) ile tüm alıcı dalgaya CFO düzeltmesi uygulanır.
+
 phase_offsets = zeros(p.Nsym, 1);
 for i = 1:p.Nsym
     start_idx = (i-1)*(p.Nfft + p.cpLength) + 1;
@@ -26,6 +32,8 @@ residual_cfo = mean(phase_offsets) / (2 * pi * p.Nfft) * p.sample_rate;
 t = (0:length(rx_data)-1).'./ p.sample_rate;
 rx_data = rx_data .* exp(-1j * 2 * pi * residual_cfo * t);
 
+%Burada normalize ve FFT'ye geçilir.
+%CP atılıp fft ile frekansa dönülür ve fftshift ile tekrar merkez-DC dizilişe geçilir. 
 rx_data = rx_data / max(abs(rx_data));
 
 rx_grid = zeros(p.Nfft, p.Nsym);
@@ -36,6 +44,8 @@ for i = 1:p.Nsym
     rx_grid(:, i) = fft_output;
 end
 
+%Her sembolde pilot konumlarındaki alma/pilot oranından kanal cevabı (H) kestirilir.
+%Veri konumlarına lineer interpolasyon ile H yayılır (alt taşıyıcı ekseninde).
 channel_grid = zeros(p.Nfft, p.Nsym);
 for i = 1:p.Nsym
     pilot_sequence = p.pilot1;
@@ -56,7 +66,7 @@ end
 
 rx_data_symbols = rx_data_symbols(:); 
 rx_bits = qamdemod(rx_data_symbols, p.M, 'OutputType', 'bit', 'UnitAveragePower', true);
-
+%qamdemod ile bitlere dön, BER ve hata sayısı hesapla.
 num_errors = sum(rx_bits ~= tx_bits);
 ber = num_errors / length(tx_bits);
 
@@ -73,4 +83,5 @@ title('Received Signal Spectrum');
 xlabel('Frequency Bin');
 ylabel('Magnitude (dB)');
 grid on;
+
 end
